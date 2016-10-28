28c125f7421b710d994480f031f09a8ed6940dc2
theJanNovak <novanojj@gmail.com>
2016-10-28 14:50:52 +0200

    basic classes

---
+++ src/main/java/cz/muni/fi/pa165/entity/Song.java
@@ -0,0 +1,10 @@
+package cz.muni.fi.pa165.entity;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by jan.novak
+ */
+@Entity
+public class Song {
+}
---
+++ src/main/java/cz/muni/fi/pa165/enums/Genre.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.pa165.enums;
+
+/**
+ * Created by jan.novak
+ */
+public enum Genre {
+}
