f1ee21492973599091156921cdb817f5419f775d
kyberpunk <xvh@seznam.cz>
2016-10-30 13:29:12 +0100

    Album DAO interface

---
+++ src/main/java/cz/muni/fi/pa165/dao/AlbumDao.java
@@ -0,0 +1,19 @@
+package cz.muni.fi.pa165.dao;
+
+import cz.muni.fi.pa165.entity.Album;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by xvh on 30.10.2016.
+ */
+public interface AlbumDao {
+    public void create(Album album);
+    public void update(Album album);
+    public void delete(Album album);
+    public Album findById(Long id);
+    public Album findByName(String name);
+    public List<Album> findAll();
+    public List<Album> findByReleaseDate(Date from, Date to);
+}
