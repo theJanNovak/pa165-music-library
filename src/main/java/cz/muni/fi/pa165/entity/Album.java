5ab339592071474c874b64716a68a941d2cde070
kyberpunk <xvh@seznam.cz>
2016-10-30 13:12:10 +0100

    Album entity

--- src/main/java/cz/muni/fi/pa165/entity/Album.java
+++ src/main/java/cz/muni/fi/pa165/entity/Album.java
@@ -1,10 +1,64 @@
 package cz.muni.fi.pa165.entity;
-
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.Date;
 
 /**
  * Created by jan.novak
  */
 @Entity
 public class Album {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String commentary;
+
+    @Temporal(TemporalType.DATE)
+    private Date releaseDate;
+
+    @Lob
+    private byte[] albumArt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCommentary() {
+        return commentary;
+    }
+
+    public void setCommentary(String commentary) {
+        this.commentary = commentary;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public byte[] getAlbumArt() {
+        return albumArt;
+    }
+
+    public void setAlbumArt(byte[] albumArt) {
+        this.albumArt = albumArt;
+    }
 }
