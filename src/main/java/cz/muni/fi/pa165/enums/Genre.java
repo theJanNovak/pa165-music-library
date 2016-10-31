89bb9ab81539692a28fa454f08909baebb75cfa0
kyberpunk <xvh@seznam.cz>
2016-10-31 16:50:37 +0100

    Genres

--- src/main/java/cz/muni/fi/pa165/enums/Genre.java
+++ src/main/java/cz/muni/fi/pa165/enums/Genre.java
@@ -4,5 +4,5 @@ package cz.muni.fi.pa165.enums;
  * Created by jan.novak
  */
 public enum Genre {
-    ROCK, POP, METAL, PUNK, PUNKROCK, COUNTRY, FOLK, JAZZ, RAP, HIPHOP, TEKNO
+    ROCK, POP, METAL, PUNK, PUNKROCK, COUNTRY, FOLK, JAZZ, RAP, HIPHOP, TEKNO, DNB
 }
